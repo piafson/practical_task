4ac0746dacd4dfed7769e0e246a34b039d8ece7a
piafson <82482136+piafson@users.noreply.github.com>
2023-07-29 16:32:41 +0800

    Add files via upload

---
+++ dbo.udf_GetSKUPrice.sql
@@ -0,0 +1,24 @@
+if OBJECT_ID('dbo.udf_GetSKUPrice') IS NOT NULL 
+    DROP FUNCTION dbo.udf_GetSKUPrice;
+go
+
+create function dbo.udf_GetSKUPrice
+(
+    @ID_SKU INT
+)
+returns decimal(18, 2)
+as
+begin
+    declare @SumValue INT;
+    declare @SumQuantity INT;
+
+    select
+        @SumValue = SUM([Value]),
+        @SumQuantity = SUM(Quantity)
+    from Basket
+    where ID_SKU = @ID_SKU
+
+    return @SumValue / @SumQuantity
+end;
+
+go
